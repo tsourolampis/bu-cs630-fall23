1a6c5aaf1ec5fff6517ef0050387902cec29b2ff
Tianyi Chen <ctianyi7@gmail.com>
2023-09-29 18:53:59 -0400

    Add files via upload

---
+++ hw2/coding.py
@@ -0,0 +1,6 @@
+def best_position(points, weights):
+  '''
+  points: List of tuples, each tuple contains two float elements corresponds to the x and y location of a point.
+  weights: List of floats.
+
+  Return: a tuple (x,y) that represents the location of the point that minimizes the weighted distance sum.
